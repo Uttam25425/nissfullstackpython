28911dc20949c228adb005f898f1c901288a18e3
Uttam <uttamsahu25425@gmail.com>
2026-02-19 11:03:38 +0530

    full stack python program

---
+++ pythonniis/__pycache__/Swap.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+print("before swapping a=",a,"b=",b)
+t=a
+a=b
+b=t
+print("after swapping a=",a,"b=",b)
---
+++ pythonniis/mark.py
@@ -0,0 +1,11 @@
+print("enter five marks")
+m1=float(input())
+m2=float(input())
+m3=float(input())
+m4=float(input())
+m5=float(input())
+total=m1+m2+m3+m4+m5
+avg=total/5
+print(m1,m2,m3,m4,m5)
+print(total)
+print(avg)
